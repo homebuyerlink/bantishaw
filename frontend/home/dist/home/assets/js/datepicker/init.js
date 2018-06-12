3ab5e0d1af9aab05532dd9d5b6df4845f9c0b4fc
zohaib545 <l134031@lhr.nu.edu.pk>
2018-06-12 11:53:44 +0500

    Folders and files renaming and moving

--- frontend/home/dist/home/assets/js/datepicker/init.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-    "use strict";
-    $(document).ready(function(){
-        $('.ct-js-datetimePicker div').datepicker({
-            todayHighlight: true
-        });
-    })
-}(jQuery));
