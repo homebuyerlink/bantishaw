d981157479524d81ab774f18cd2644594d896f08
MZeeDev <ziaullah623@gmail.com>
2018-06-13 17:18:37 +0500

    guards activated

--- frontend/client/src/app/guards/guest.guard.ts
+++ frontend/client/src/app/guards/guest.guard.ts
@@ -2,14 +2,10 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthenticationService } from '../services/authentication.service';
 
-
-
-
-
 @Injectable()
+
 export class GuestGuard implements CanActivate {
   constructor(private authService: AuthenticationService, private router: Router) { }
-
   async canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot) {
