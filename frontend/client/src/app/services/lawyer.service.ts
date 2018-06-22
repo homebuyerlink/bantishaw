07d9502e234e022393d9913c967d9b77371be23e
Haseeb Ahmad <haseebahmad007@hotmail.com>
2018-06-22 20:39:39 +0500

    tasks

--- frontend/client/src/app/services/lawyer.service.ts
+++ frontend/client/src/app/services/lawyer.service.ts
@@ -12,7 +12,10 @@ export class LawyerService {
       companyName: companyName, lawyerName: lawyerName, designation: designation, addressLine1: addressLine1, addressLine2: addressLine2, city: city, state: state, zip: zip, phone: phone, email: email, website: website, founded: founded, image: image, lat: lat, lng: lng, radius: radius, tags: tags, userId: userId, facebook: facebook, youtube: youtube, instagram: instagram, gplus: gplus, twitter: twitter, associations: associations
     };
     console.log(body);
+  }
 
+  setService(services) {
+    console.log(services);
   }
 
 }
