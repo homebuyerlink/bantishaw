2d59b5d739289813cbdc8311576b5bd1f254bd5a
zohaib545 <l134031@lhr.nu.edu.pk>
2018-07-02 01:05:49 +0500

    config url changed

--- frontend/client/src/app/config.ts
+++ frontend/client/src/app/config.ts
@@ -3,8 +3,8 @@ import { HttpHeaders } from "@angular/common/http";
 let httpHeaders: HttpHeaders = new HttpHeaders({ 'Content-type': 'application/json', 'Authorization': `bearer ${localStorage.getItem('token')}` });
 
 export const Config = Object.freeze({
-    API_BASE: "http://localhost:3000/api",
-    // API_BASE: "https://home-buyer.herokuapp.com/api",
+    // API_BASE: "http://localhost:3000/api",
+    API_BASE: "https://home-buyer.herokuapp.com/api",
     HEADERS: { headers: httpHeaders },
     refresh: function () {
         httpHeaders = new HttpHeaders({ 'Content-type': 'application/json', 'Authorization': `bearer ${localStorage.getItem('token')}` });
